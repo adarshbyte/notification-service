2ff048fe470c9db9626a0d0e468a5cb862bafbfc
adarshbyte <rajadarshofficial@gmail.com>
2025-08-13 06:59:13 +0530

    added notify send api endpoint

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,26 @@
 import express from 'express';
-import {notifyQueue} from './queue';
+import {notifyQueue} from './queue.js';
 const app = express();
-
+app.use(express.json())
 const PORT = 8000;
+
+app.post('/notify/send',async (req,res)=>{
+    try{
+        const {userId="adarsh",message="message"} = req.body;
+        const jobId = userId+message;
+        await notifyQueue.add('send-message',{userId,message},{jobId,
+             removeOnComplete: { age: 24*3600 },  
+             removeOnFail:     { age: 7*24*3600 }
+        })
+        res.status(200).json({msg:'success'});
+    }catch(e){
+        res.status(500).json({msg:'error'});
+    }
+})
 
 app.get('/',(_req,res)=>{
     res.send('HELLOW FROM TYPESCRIPT + EXPRESSS')
-   
+    
     notifyQueue.add('/send-msg',{
         phone:'9508103357',
         message:'hello bhai! bull mq ka job agya'
